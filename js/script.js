674754622d973a485c2fd0341a16c6ece76e9a0d
Andres <anondiaz@gmail.com>
2025-04-07 13:36:24 +0200

    avanzando

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Validación del formulario
+let inputNombre = document.querySelector('#nombre')
+let errorNombre = document.querySelector('#errorNombre')
+const mensajeErrorNombre = "<span>&#9888;</span> Error: por favor, escriba un nombre válido"
+
+inputNombre.addEventListener('change', () => {
+
+    let nombre = inputNombre.value.trim()
+
+    if(nombre.length < 2) {
+        inputNombre.value = ""
+        errorNombre.innerHTML = mensajeErrorNombre
+        inputNombre.setAttribute('aria-describedby', "errorNombre")        
+        inputNombre.focus()
+    }
+})
+
+
+let inputOpinion = document.getElementById('opinion')
+let errorOpinion = document.getElementById('errorOpinion')
+const mensajeErrorOpinion = "<span>&#9888;</span> Por favor, necesitamos que su opinión tenga más de 20 caracteres."
+
+inputOpinion.addEventListener('change', () => {
+    let opinion = inputOpinion.value.trim()
+
+    if (opinion.length < 20) {
+        errorOpinion.innerHTML = mensajeErrorOpinion
+        inputOpinion.setAttribute('aria-describedby', "errorOpinion")   
+        inputOpinion.focus()  
+    }
+})
